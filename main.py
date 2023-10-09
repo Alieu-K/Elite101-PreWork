8216bcd438538b1d37b139c984df26bea604332b
SosaAli <136405855+SosaAli@users.noreply.github.com>
2023-10-09 22:57:04 +0000

    Add an add_order feature to get started on making a customer's order.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 # imports
 
 # Define Functions
+def order_maker():
+  x = True
+  main_dishes = ["Burger", "Burger 2.0", "Cheeseburger", "Vegan Burger","Lettuce  Burger"]
+  while x is True:
+    print("\n--------------------------------------------------------\n")
+    print("What main would you like on the menu?\n")
+    for dish in main_dishes:
+      print(dish)
+      print("-------------------\n")
+    meal_part_one = input("")
+    if meal_part_one in main_dishes:
+      print("Ok, noted.")
+      x = False
+    else:
+      print("Please type an item on the list")
+  
 
 def user_query():
   user_choice = True
@@ -9,24 +25,29 @@ def user_query():
     print("\nHow may I assit you?")
     
     print("-------------------")
-    print("PlaceHolder 1")
+    print("Make an Order(1)")
     print("-------------------")
-    print("PlaceHolder 2")
+    print("Edit the Order(2)")
     print("-------------------")
-    print("PlaceHolder 3")
+    print("See Order Status(3)")
     print("-------------------")
-    print("PlaceHolder 4")
+    print("See Reciept(4)")
     print("-------------------")
-    print("Exit(5)")
+    print("Date/Time Check(5)")
+    print("-------------------")
+    print("Exit(6)")
 
     choice = input("Type each chocie respective number to select that choice. ")
 
-    if choice == "5":
+    if choice == "6":
       print(f"Bye bye {name}")
       user_choice = False
 
+    if choice == "1":
+      order_maker()
+
 # Program itself
-print("Internet's Handy Chatbot")
+print("Hot Burgers Chatbot")
 print("------------------------------------------")
 
 name = input("What is your name? ")
@@ -52,4 +73,9 @@ print(f"Hello {name}, welcome to the Internet's Handy Chatbot.")
 user_query()
 
 
-  
+"""
+Sources:
+
+https://stackoverflow.com/questions/7713700/check-variable-if-it-is-in-a-list | Used for the "in" operator.
+
+"""
